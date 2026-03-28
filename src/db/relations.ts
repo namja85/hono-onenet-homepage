f74ac0f12f879d5223951b818d3406ff3b939b53
evan-hyun <hyun25@onedaybiotech.com>
2026-03-28 17:11:23 +0900

    better-auth 설치 및 관련 스키마 추가

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -8,6 +8,16 @@ const relations = defineRelations(schema, (r) => ({
   files: {
     board: r.one.boards({ from: r.files.boardId, to: r.boards.id }),
   },
+  user: {
+    sessions: r.many.session({ from: r.user.id, to: r.session.userId }),
+    accounts: r.many.account({ from: r.user.id, to: r.account.userId }),
+  },
+  session: {
+    user: r.one.user({ from: r.session.userId, to: r.user.id }),
+  },
+  account: {
+    user: r.one.user({ from: r.account.userId, to: r.user.id }),
+  },
 }));
 
 export default relations;
